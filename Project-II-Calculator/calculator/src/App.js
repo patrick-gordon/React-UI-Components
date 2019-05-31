68addf0009b899bd0d9f423bd99e29dfb1f1042b
Patrick Gordon <patrickgordon42@yahoo.com>
2019-05-30 22:47:09 -0500

    working on it

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import './App.css';
+import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import CalcDisplay from './components/DisplayComponents/CalculatorDisplay';
 
-const App = () => {
+const numbers = [7,8,9,4,5,6,1,2,3];
+const signs = ['/', 'x', '-', '+', '='];
+
+
+
+
+const App= () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
+   <div className='calculator-container'>
+    <CalcDisplay displayStyle='display' text='0'/>
+    
+    
+
+     
+    
+  </div>
   );
-};
+}
 
 export default App;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+const CalcDisplay = (props) => {
+    return(
+        <div className={props.buttonStyle}> {props.text} </div>
+    );
+}
+
+export default CalcDisplay
