786d00c93c2dd645e0a6925741802733defb1b0a
Patrick Gordon <patrickgordon42@yahoo.com>
2019-06-03 18:52:12 -0500

    got all except the icons

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -5,7 +5,7 @@ import './Header.css';
 function HeaderContent(){
     return(
       <div className='header-content'>
-        <p>Lets learn React by building simple interfaces with componenets. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using componenetsyou are well on your way to mastering React!</p>
+        <p>Lets learn React by building simple interfaces with componenets. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using componenets you are well on your way to mastering React!</p>
       </div>
     )
 }
